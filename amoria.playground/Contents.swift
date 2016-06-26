037bbea37986287e23f3fc61b3220f31fdc59584
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-06-25 19:00:53 -0700

    updated amoria

--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -4,19 +4,6 @@
  */
 
 
-// Initialize the Array
-var a = [Int]()
-
-print(a.count)
-
-// Append another array
-a += [4,5,6]
-print(a)
-
-let increment = 5
-
-for i in 1...20  {
-    print(i)
-    var i = i + increment
+public enum yo {
+    static let 
 }
-		
