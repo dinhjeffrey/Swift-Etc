27ca1e8a65e4da5a8109ddc744cbc5a29a6959b1
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-01 09:59:24 -0700

    updated protocols playground

--- protocols.playground/Contents.swift
+++ protocols.playground/Contents.swift
@@ -26,3 +26,49 @@ var fordF150 = Vehicle()
 fordF150.name = "Ford F-150"
 fordF150.flies = false
 print(fordF150.fly(150))
+
+// Stanford
+
+protocol Moveable {
+    mutating func moveTo(p: CGPoint)
+}
+class Car: Moveable {
+    func moveTo(p: CGPoint) {
+        print("...")
+    }
+    func changeOil() {}
+}
+struct Shape: Moveable {
+    mutating func moveTo(p: CGPoint) {
+        print("...")
+    }
+    func draw() {}
+}
+let prius: Car = Car()
+let square: Shape = Shape()
+
+var thingsToMove: Moveable = prius
+thingsToMove.moveTo(CGPoint(x: 1.0, y: 1.0))
+//thingsToMove.changeOil()                // error out because thingsToMove is of Type Moveable, not Type Car
+
+thingsToMove = square
+let thingsToMoveArray: [Moveable] = [square, prius]
+
+func slide(slider: Moveable) {
+    var slider = slider
+    let positionToSlideTo = CGPoint(x: 2.0, y: 2.0)
+    slider.moveTo(positionToSlideTo)
+}
+slide(prius)
+slide(square)
+func slipAndSlide(x: protocol<Moveable>) {}
+slipAndSlide(prius)
+
+
+
+
+
+
+
+
+
