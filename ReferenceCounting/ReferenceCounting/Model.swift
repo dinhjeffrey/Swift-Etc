c9a92d7b4c377895f67d8d4c5097b2ac19263d69
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-01 09:58:44 -0700

    added Reference Counting, for memory cycle

---
+++ ReferenceCounting/ReferenceCounting/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  ReferenceCounting
+//
+//  Created by jeffrey dinh on 6/30/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import Foundation
---
+++ ReferenceCounting/ReferenceCounting/ReferenceCountingVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  ReferenceCounting
+//
+//  Created by jeffrey dinh on 6/30/16.
+//  Copyright © 2016 jeffrey dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+//    private var count = 0
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        count += 1
+//        print("Loaded up a new MVC  (count is \(count))")
+//    }
+//    
+//    deinit {
+//        count -= 1
+//        print("Left the heap  (count is \(count))")
+//    }
+//    
+
+
+}
+
