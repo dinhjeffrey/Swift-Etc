1ff64888876bff428db5665c477602286f81d3a6
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-02 14:26:38 -0700

    updated extensions playground

--- extensions.playground/Contents.swift
+++ extensions.playground/Contents.swift
@@ -35,3 +35,46 @@ var numberOfAs: Int = "a random string".letterOccurrence("a")
 print(numberOfAs)
 
 
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+let oneInch = 25.4.mm
+print("One inch is \(oneInch) meters")
+// Prints "One Inch is 0.0254 meters"
+let threeFeet = 3.ft
+print("Three feet is \(threeFeet) meters")
+// Prints "Three feet is 0.9143999807 meters"
+
+//NOTE: Extensions can add new computed properties, but they cannot add stored properties, or add property observers to existing properties
+// Initializers: Extensions can add new convenience initializers to a class, but cannot add new designated initializers or deinitializers to a class
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y=0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+
+
+
+
+
+
+
+
+
+
+
