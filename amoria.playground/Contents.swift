eea89f0b4ab6721e13044adfea38fe0bd3a50177
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-05 23:35:15 -0700

    updated amoria

--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -4,6 +4,25 @@
  */
 
 
-var integer = [1,2,3] // index max is 2
 
-integer.count - 1
+func checkPalindrome(str: String) -> Bool {
+    guard str != "" || str.characters.count != 1 else { return true }
+    return str == String(str.characters.reverse())
+}
+
+checkPalindrome("racecar")
+checkPalindrome("p")
+
+
+
+func combineSortedArrays(arr1: [Int], arr2: [Int]) -> [Int] {
+    var newArr = [Int]()
+    for (index, element) in arr1.enumerate() {
+        if element < arr2[index] {
+            newArr.append(element)
+        } else {
+            newArr.append(arr2[index])
+        }
+    }
+}
+
