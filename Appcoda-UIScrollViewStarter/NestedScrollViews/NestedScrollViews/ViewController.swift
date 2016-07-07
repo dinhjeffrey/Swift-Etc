8b699e4e4a3176272b5514c74b41cd73fe420a34
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-07 08:12:48 -0700

    added parallax effect by adding a method that calculates the percentageScroll from the foreground scrollView and offsets the background scrollView

--- Appcoda-UIScrollViewStarter/NestedScrollViews/NestedScrollViews/ViewController.swift
+++ Appcoda-UIScrollViewStarter/NestedScrollViews/NestedScrollViews/ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// We’ll need to know when the foreground view is scrolled, so we can calculate the amount the background should scroll by and use this value to scroll it. We’ll use a UIScrollViewDelegate method for this.
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var background: UIScrollView!
+    @IBOutlet weak var foreground: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        foreground.delegate = self
+    }
+    // In the above code, we get the height of the foreground and calculate by how much the foreground has been scrolled. We then take this value and multiply it with the background’s height and use this in setting the background’s contentOffset which will result with the background moving a little bit faster than the foreground, each time the foreground is scrolled. Run the app and you’ll see the parallax effect created by this.
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let foregroundHeight = foreground.contentSize.height - CGRectGetHeight(foreground.bounds)
+        let percentageScroll = foreground.contentOffset.y / foregroundHeight
+        let backgroundHeight = background.contentSize.height - CGRectGetHeight(background.bounds)
+        
+        background.contentOffset = CGPoint(x: 0, y: backgroundHeight * percentageScroll)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
