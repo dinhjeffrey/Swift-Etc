98f4c08f951fdaa66cc61c5b1f9d4bf673a1232e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-07 20:34:28 -0700

    created serialQueue so images download one at a time, in order

--- AppCoda-ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
+++ AppCoda-ConcurrencyDemo/ConcurrencyDemo/ViewController.swift
@@ -38,26 +38,27 @@ class ViewController: UIViewController {
     
     @IBAction func didClickOnStart(sender: AnyObject) {
         // We first get a reference to the default concurrent queue using dispatch_get_global_queue, then inside the block we submit a task which is to download the first image. Once the image download completes, we submit another task to the main queue to update the image view with the downloaded image. In other words, we put the image download task in a background thread, but execute the UI related tasks in the main queue.
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-        dispatch_async(queue) {
+        let _ = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        let serialQueue = dispatch_queue_create("com.appcoda.imagesQueue", DISPATCH_QUEUE_SERIAL)
+        dispatch_async(serialQueue) {
             let img1 = Downloader.downloadImageWithURL(imageURLs[0])
             dispatch_async(dispatch_get_main_queue()) {
                 self.imageView1.image = img1
             }
         }
-        dispatch_async(queue) {
+        dispatch_async(serialQueue) {
             let img2 = Downloader.downloadImageWithURL(imageURLs[1])
             dispatch_async(dispatch_get_main_queue()) {
                 self.imageView2.image = img2
             }
         }
-        dispatch_async(queue) {
+        dispatch_async(serialQueue) {
             let img3 = Downloader.downloadImageWithURL(imageURLs[2])
             dispatch_async(dispatch_get_main_queue()) {
                 self.imageView3.image = img3
             }
         }
-        dispatch_async(queue) {
+        dispatch_async(serialQueue) {
             let img4 = Downloader.downloadImageWithURL(imageURLs[3])
             dispatch_async(dispatch_get_main_queue()) {
                 self.imageView4.image = img4
