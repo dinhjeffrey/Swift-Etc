0d4fe62a24fece9a5d1916284af6bafdc88ad4e1
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-07 23:44:13 -0700

    added UITableView playground

---
+++ UITableView.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// UITableView
+
+// UITableView Protocols
+// Connections to code are made using the UITableView's dataSource and delegate
+// The delegate is used to control HOW the table is displayed (it's look and feel)
+// The dataSource provides THE DATA that is displayed inside the cells
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
