55cce11675d0fc9405e41c04949523c7326c658e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-09 16:46:16 -0700

    up to loading of cell descriptions working

--- AppCoda-Expandable-TableView/Expandable/ViewController.swift
+++ AppCoda-Expandable-TableView/Expandable/ViewController.swift
@@ -9,7 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    // This two-dimensional array will store the index of the visible cells for each section (one dimension for the sections, one for the rows).
+    var visibleRowsPerSection = [[Int]]()
+    // implementing visible cell function
+    func getIndicesOfVisibleRows() {
+        visibleRowsPerSection.removeAll()
+        
+        for currentSectionCells in cellDescriptors {
+            var visibleRows = [Int]()
+            
+            for row in 0...((currentSectionCells as! [[String: AnyObject]]).count - 1) {
+                if currentSectionCells[row]["isVisible"] as! Bool == true {
+                    visibleRows.append(row)
+                }
+            }
+            
+            visibleRowsPerSection.append(visibleRows)
+        }
+    }
+    
+    // This array will contain all the cell description dictionaries that will be loaded from the property list file.
+    var cellDescriptors: NSMutableArray!
+    // new custom function that’s responsible for loading the file contents into the array
+    func loadCellDescriptors() {
+        if let path = NSBundle.mainBundle().pathForResource("CellDescriptor", ofType: "plist") {
+            cellDescriptors = NSMutableArray(contentsOfFile: path)
+            getIndicesOfVisibleRows()
+            tblExpandable.reloadData()
+        }
+    }
+    
+    
     // MARK: IBOutlet Properties
     
     @IBOutlet weak var tblExpandable: UITableView!
@@ -25,6 +55,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewWillAppear(animated)
         
         configureTableView()
+        loadCellDescriptors()
     }
     
     
