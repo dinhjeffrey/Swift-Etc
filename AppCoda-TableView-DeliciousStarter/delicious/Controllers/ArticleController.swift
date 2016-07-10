fdde6ef96cd9493545b22dea2779e55a9dce3df5
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-10 14:06:10 -0700

    first commit deliciousStarter UITableView

---
+++ AppCoda-TableView-DeliciousStarter/delicious/Controllers/ArticleController.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleController.swift
+//  Delicious
+//
+//  Created by Sean Choo on 4/27/16.
+//  Copyright © 2016 Demo. All rights reserved.
+//
+
+import UIKit
+
+class ArticleController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
