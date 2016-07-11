ac14ecf9cfdea5948764914a8d8e6eba84e4013b
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-11 11:31:28 -0700

    added parseCSV method

--- AppCoda-CoreDataPreloadDemoStart/CoreDataPreloadDemo/AppDelegate.swift
+++ AppCoda-CoreDataPreloadDemoStart/CoreDataPreloadDemo/AppDelegate.swift
@@ -11,12 +11,87 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func parseCSV (contentsOfURL: NSURL, encoding: NSStringEncoding, error: NSErrorPointer) -> [(name:String, detail:String, price: String)]? {
+        // Load the CSV file and parse it
+        let delimiter = ","
+        var items:[(name:String, detail:String, price: String)]?
+        
+        do {
+            let content = try String(contentsOfURL: contentsOfURL, encoding: encoding)
+            print(content)
+            let lines: [String] = content.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet()) as [String]
+            
+            for line in lines {
+                var values: [String] = []
+                if line != "" {
+                    // For a line with double quotes
+                    // we use NSScanner to perform the parsing
+                    if line.rangeOfString("\"") != nil {
+                        var textToScan: String = line
+                        var value: NSString?
+                        var textScanner: NSScanner = NSScanner(string: textToScan)
+                        while textScanner.string != "" {
+                            if (textScanner.string as NSString).substringToIndex(1) == "\"" {
+                                textScanner.scanLocation += 1
+                                textScanner.scanUpToString("\"", intoString: &value)
+                                textScanner.scanLocation += 1
+                            } else {
+                                textScanner.scanUpToString(delimiter, intoString: &value)
+                            }
+                            
+                            // Store the value into the values array
+                            values.append(value as! String)
+                            
+                            // Retrieve the unscanned remainder of the string
+                            if textScanner.scanLocation < textScanner.string.characters.count {
+                                textToScan = (textScanner.string as NSString).substringFromIndex(textScanner.scanLocation + 1)
+                            } else {
+                                textToScan = ""
+                            }
+                            textScanner = NSScanner(string: textToScan)
+                        }
+                        // For a line without double quotes, we can simply seperate the string
+                        // by using the delimiter (e.g. comma)
+                    } else {
+                        values = line.componentsSeparatedByString(delimiter)
+                    }
+                    
+                    // Put the values into the tuple and add it to the items array
+                    let item = (name: values[0], detail: values[1], price: values[2])
+                    items?.append(item)
+                }
+            }
+        } catch {
+            print(error)
+        }
+       
+        return items
+    }
+    
+ 
+    func preloadData () {
+        // Retrieve data from the source file
+       
+    }
+    
+    func removeData () {
+        // Remove the existing items
+
+        
+    }
+    
+    
+    
+    
+    
+    
 
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
+        preloadData()
         return true
     }
 
--- AppCoda-CoreDataPreloadDemoStart/CoreDataPreloadDemo/MenuTableViewController.swift
+++ AppCoda-CoreDataPreloadDemoStart/CoreDataPreloadDemo/MenuTableViewController.swift
@@ -10,25 +10,25 @@ import UIKit
 import CoreData
 
 class MenuTableViewController: UITableViewController {
-
+    
     private var menuItems:[MenuItem] = []
     var fetchResultController:NSFetchedResultsController!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load menu items from database
-        if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-
-            let fetchRequest = NSFetchRequest(entityName: "MenuItem")
-            do {
-                menuItems = try managedObjectContext.executeFetchRequest(fetchRequest) as! [MenuItem]
-            } catch {
-                print("Failed to retrieve record")
-                print(error)
-            }
+        let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext
+        
+        let fetchRequest = NSFetchRequest(entityName: "MenuItem")
+        do {
+            menuItems = try managedObjectContext!.executeFetchRequest(fetchRequest) as! [MenuItem]
+        } catch {
+            print("Failed to retrieve record")
+            print(error)
         }
-
+        
+        
         // Make the cell self size
         tableView.estimatedRowHeight = 66.0
         tableView.rowHeight = UITableViewAutomaticDimension
@@ -38,38 +38,38 @@ class MenuTableViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // Return the number of sections.
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
         return menuItems.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! MenuTableViewCell
-
+        
         // Configure the cell...
         cell.nameLabel.text = menuItems[indexPath.row].name
         cell.detailLabel.text = menuItems[indexPath.row].detail
         cell.priceLabel.text = "$\(menuItems[indexPath.row].price as! Double)"
-
+        
         return cell
     }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
