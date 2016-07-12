28e218e025daec8160832ce3e87eb83053f463a4
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-12 13:24:27 -0700

    populate data in table working

--- AppCoda-NSTimer-PullRefresh/PullRefresh/ViewController.swift
+++ AppCoda-NSTimer-PullRefresh/PullRefresh/ViewController.swift
@@ -8,21 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tblDemo: UITableView!
+    
+    var dataArray: Array<String> = ["One", "Two", "Three", "Four", "Five"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tblDemo.delegate = self
+        tblDemo.dataSource = self
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("idCell", forIndexPath: indexPath) 
+        
+        cell.textLabel!.text = dataArray[indexPath.row]
+        
+        return cell
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
