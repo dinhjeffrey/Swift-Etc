6258d088ebfa88beb660558a2fa9251cfde1eeaf
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-12 16:28:45 -0700

    example 1 done

--- AppCoda View Animations Demo/View Animations Demo/ExampleIViewController.swift
+++ AppCoda View Animations Demo/View Animations Demo/ExampleIViewController.swift
@@ -9,6 +9,51 @@
 import UIKit
 
 class ExampleIViewController: UIViewController {
+    
+    @IBOutlet weak var centerAlignUsername: NSLayoutConstraint!
+    @IBOutlet weak var centerAlignPassword: NSLayoutConstraint!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func login(sender: UIButton) {
+        let bounds = self.loginButton.bounds
+        UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {
+            self.loginButton.bounds = CGRect(x: bounds.origin.x - 20, y: bounds.origin.y, width: bounds.size.width + 60, height: bounds.size.height)
+            self.loginButton.enabled = false
+            }, completion: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        centerAlignUsername.constant -= view.bounds.width
+        centerAlignPassword.constant -= view.bounds.width
+        loginButton.alpha = 0.0
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // layoutIfNeeded() is used to lay out the views immediately they are changed. If you don’t include it, you will not see them get animated onto the screen, instead they will just be shown in their final position.
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { 
+            self.centerAlignUsername.constant += self.view.bounds.width
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        UIView.animateWithDuration(0.5, delay: 0.3, options: .CurveEaseOut, animations: {
+            self.centerAlignPassword.constant += self.view.bounds.width
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+        UIView.animateWithDuration(0.5, delay: 0.4, options: .CurveEaseOut, animations: {
+            self.loginButton.alpha = 1
+            }, completion: nil)
+    }
+    /*
+     The following list shows the animatable properties.
+     
+     center
+     alpha
+     frame
+     bounds
+     transform
+     backgroundColor
+     contentStretch
+     */
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +61,49 @@ class ExampleIViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
