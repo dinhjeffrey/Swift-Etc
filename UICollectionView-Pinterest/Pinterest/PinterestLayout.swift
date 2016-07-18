87b5cf4dabd1ef9c9344ca1b11d9b33151574b6c
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-18 15:17:00 -0700

    added UICollectionViewLayout custom class

---
+++ UICollectionView-Pinterest/Pinterest/PinterestLayout.swift
@@ -0,0 +1,13 @@
+//
+//  PinterestLayout.swift
+//  Pinterest
+//
+//  Created by jeffrey dinh on 7/18/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PinterestLayout: UICollectionViewLayout {
+
+}
