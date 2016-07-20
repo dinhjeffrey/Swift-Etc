bc2308c25fcedd815255f5b19da7894d1d948f25
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-20 09:26:39 -0700

    tested longest collatz sequence

--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -3,26 +3,45 @@
  (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧            (づ￣ ³￣)づ                    (づ｡◕‿‿◕｡)づ
  */
 
+// LONGEST COLLATZ SEQUENCE
 
-
-func checkPalindrome(str: String) -> Bool {
-    guard str != "" || str.characters.count != 1 else { return true }
-    return str == String(str.characters.reverse())
+// method calculates how many steps it takes for a number to follow the collatz sequence (go to 1 using the even/odd technique). stores the amount of steps it takes corresponding to the index number in the array
+private func collatzLength(number: Int, inout cache: [Int]) -> Int {
+    var n = number
+    var steps = 0
+    
+    while n >= number { // if goes below starting number, we looking into the cache array where we store the amount of steps to the number for all previous numbers before this number
+        if n % 2 == 0 { // if even divide by 2
+            n /= 2
+        } else {
+            n = (3*n + 1) / 2 // if odd perform 2 steps, an odd then an even, because it will become even
+            steps += 1
+        }
+        steps += 1
+    }
+    steps += cache[n] // since we early exit before 1, we add the amount of steps we calculate previously for n
+    cache[number] = steps // add amount of steps for original number, by indexing into the array
+    
+    return steps
 }
 
-checkPalindrome("racecar")
-checkPalindrome("p")
-
-
-
-func combineSortedArrays(arr1: [Int], arr2: [Int]) -> [Int] {
-    var newArr = [Int]()
-    for (index, element) in arr1.enumerate() {
-        if element < arr2[index] {
-            newArr.append(element)
-        } else {
-            newArr.append(arr2[index])
+// method takes in maximum number to find out the longest chain
+private func longestCollatzSequence(maxNumber: Int) -> (number: Int, steps: Int) {
+    var longestCollatzSequence = (number: 0, steps: 0) // initiate a tuple to store the longest sequence
+    var cache = [Int](count: maxNumber, repeatedValue: 0) // initiate an array of size maxNumber to store the amount of steps for each number
+    cache[1] = 1 // number 1 has 1 step, we count that too
+    
+    guard maxNumber != 2 else {return (1, 1)} // doesn't enter loop if maxNumber is 2, so we set it to (1, 1) since #1 has 1 step
+    
+    for number in 2..<maxNumber { // we use less than sign here so we can put 1_000_000 and not include it
+        let calculatedSteps = collatzLength(number, cache: &cache)
+        if calculatedSteps > longestCollatzSequence.steps { // compare to one value of the tuple here
+            longestCollatzSequence = (number: number, steps: calculatedSteps)
         }
     }
+    
+    return longestCollatzSequence
 }
+
+longestCollatzSequence(5)
 
