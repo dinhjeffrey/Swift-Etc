8d5a3c9e162cc868eb11392d270398b1a9785cf4
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-27 08:43:58 -0700

    update amoria

--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -2,46 +2,100 @@
  Ｏ(≧▽≦)Ｏ	              ❤ Amoria ❤                       ★~(◡﹏◕✿)
  (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧            (づ￣ ³￣)づ                    (づ｡◕‿‿◕｡)づ
  */
+import Foundation
+// Dynamic Array
 
-// LONGEST COLLATZ SEQUENCE
 
-// method calculates how many steps it takes for a number to follow the collatz sequence (go to 1 using the even/odd technique). stores the amount of steps it takes corresponding to the index number in the array
-private func collatzLength(number: Int, inout cache: [Int]) -> Int {
-    var n = number
-    var steps = 0
-    
-    while n >= number { // if goes below starting number, we looking into the cache array where we store the amount of steps to the number for all previous numbers before this number
-        if n % 2 == 0 { // if even divide by 2
-            n /= 2
-        } else {
-            n = (3*n + 1) / 2 // if odd perform 2 steps, an odd then an even, because it will become even
-            steps += 1
-        }
-        steps += 1
-    }
-    steps += cache[n] // since we early exit before 1, we add the amount of steps we calculate previously for n
-    cache[number] = steps // add amount of steps for original number, by indexing into the array
-    
-    return steps
+func readIntegers() -> [Int] {
+    return readLine()!.componentsSeparatedByString(" ").map{ Int($0)! }
 }
 
-// method takes in maximum number to find out the longest chain
-private func longestCollatzSequence(maxNumber: Int) -> (number: Int, steps: Int) {
-    var longestCollatzSequence = (number: 0, steps: 0) // initiate a tuple to store the longest sequence
-    var cache = [Int](count: maxNumber, repeatedValue: 0) // initiate an array of size maxNumber to store the amount of steps for each number
-    cache[1] = 1 // number 1 has 1 step, we count that too
-    
-    guard maxNumber != 2 else {return (1, 1)} // doesn't enter loop if maxNumber is 2, so we set it to (1, 1) since #1 has 1 step
-    
-    for number in 2..<maxNumber { // we use less than sign here so we can put 1_000_000 and not include it
-        let calculatedSteps = collatzLength(number, cache: &cache)
-        if calculatedSteps > longestCollatzSequence.steps { // compare to one value of the tuple here
-            longestCollatzSequence = (number: number, steps: calculatedSteps)
-        }
+var input = readIntegers()
+// sets 2 constants in one line
+let (numOfSequences, numOfQueries) = (input[0], input[1])
+var lastAns = 0
+// initialize an array with a count of numOfSequences nested empty arrays inside
+var seqList = [[Int]](count: numOfSequences, repeatedValue: [])
+
+for _ in 0..<numOfQueries {
+    let input = readIntegers()
+    let (type, x, y) = (input[0], input[1], input[2])
+    // formula to find particular sequence in seqList
+    let seqListIndex = (x ^ lastAns) % numOfSequences
+    // type can only be 1 or 2
+    switch type {
+    case 1:
+        seqList[seqListIndex].append(y)
+    case 2:
+        let seqIndex = y % seqList[seqListIndex].count
+        // find element inside sequence at index calculated right above and set to lastAns
+        lastAns = seqList[seqListIndex][seqIndex]
+        print(lastAns)
+    default:
+        fatalError("no type found")
     }
-    
-    return longestCollatzSequence
 }
 
-longestCollatzSequence(5)
+
+
+
+
+// 2D array
+func readIntegers() -> [Int] {
+    return readLine()!.componentsSeparatedByString(" ").map{ Int($0)! }
+}
+let rows = 6
+let cols = 6
+var twoDarray = [[Int]](count: rows, repeatedValue: [])
+// creates the 2d array from input
+for index in 0..<rows {
+    twoDarray[index] = readIntegers()
+}
+
+// method calculates sum for a single hourglass
+func hourglassSum(input: [[Int]], atRow row: Int, atCol col: Int) -> Int {
+    var sum = input[row][col] + input[row][col+1] + input[row][col+2]
+    sum += input[row+1][col+1]
+    sum += input[row+2][col] + input[row+2][col+1] + input[row+2][col+2]
+    return sum
+}
+
+func maxHourglassSum(input:[[Int]]) -> Int {
+    var maxSum = -100 // 7 * -9 = -63
+    for row in 0..<rows-2 {
+        for col in 0..<cols-2 {
+            let sum = hourglassSum(input, atRow: row, atCol: col)
+            if sum > maxSum {
+                maxSum = sum
+            }
+        }
+    }
+    return maxSum
+}
+print(maxHourglassSum(twoDarray))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
