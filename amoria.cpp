e6fcd5c72bef56b5797c5ba655680b560f32ebd7
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-07-29 14:21:49 -0700

    updates amoria

--- amoria.cpp
+++ amoria.cpp
@@ -1,31 +1,169 @@
-// Print the elements of a Linked List
-void Print(Node *head) {
-    while (head != NULL) {
-        cout<< head->data <<endl;
-        head = head->next;
+// Insert a node at the head of a linked list
+Node* Insert(Node *head,int data) {
+    // creates new node
+    Node *newNode = new Node;
+    // sets data
+    newNode->data = data;
+    // sets next link
+    newNode->next = head;
+    // updates head to newNode
+    head = newNode;
+    return head;
+}
+
+// Insert a node at a specific position in a linked list
+Node* InsertNth(Node *head, int data, int position) {
+    // creates a new node
+    Node *newNode = new Node;
+    // sets data
+    newNode->data = data;
+    // sets next
+    newNode->next = NULL;
+    // if position is at the beginning
+    if (position == 0) {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+    // create a temp to get node at position
+    Node *temp = head;
+    // loop until we get the node right before the position node
+    for(int i=0; i<position-1; i+=1) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = temp->next->next;
+    return head;
+}
+
+// Delete a Node
+Node* Delete(Node *head, int position)
+{
+    // create a temp
+    Node *temp = head;
+    // loop through to node right before position's node
+    for (int i=0; i<position-1; i+=1) {
+        temp = temp->next;
+    }
+    // del is node to delete
+    Node *del = temp->next;
+    temp->next = del->next;
+    delete(del);
+    return head;
+}
+
+// Print in reverse
+void ReversePrint(Node *head)
+{
+    if (head == NULL) {
+        return;
+    }
+    ReversePrint(head->next);
+    cout<<head->data<<endl;
+}
+
+// Reverse a linked list
+Node* Reverse(Node *head)
+{
+    // creates nodes
+    Node *curr, *prev, *next;
+    // sets curr
+    curr = head;
+    // sets prev
+    prev = NULL;
+    // loops through list until curr is at last node
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+    return head;
+}
+
+// Compare two linked lists
+int CompareLists(Node *headA, Node* headB)
+{
+    // to return from recusive
+    if (headA == NULL && headB == NULL) {
+        return true;
+    }
+    if (headA != NULL && headB != NULL) {
+        // recusive function that checks data and loops through each link list
+        return (headA->data == headB->data && CompareLists(headA->next, headB->next));
+    }
+    // hit here if one list is empty
+    return false;
+}
+
+// Merge two sorted linked lists
+Node* MergeLists(Node *headA, Node* headB)
+{
+    // if headA list is empty
+    if (headA == NULL) {
+        return headB;
+    }
+    // if headB list is empty
+    if (headB == NULL) {
+        return headA;
+    }
+    // if the data in headA is smaller
+    if (headA->data <= headB->data) {
+        // execute recursive function
+        // sets next link in headA to recursive function
+        headA->next = MergeLists(headA->next, headB);
+        // populate data in appropriate link after recursive function
+        return headA;
+    } else {
+        headB->next = MergeLists(headA, headB->next);
+        return headB;
     }
 }
 
-// Insert a Node at the Tail of a Linked List
-Node* Insert(Node *head, int data) {
-    // creates a temp to hold the last node and set last's data and next
-    Node *last = new Node;
-    last->data = data;
-    last->next = NULL;
-    // if the linked list is empty then set head = last
-    if (head == NULL) {
-        head = last;
-    } else { // if the linked list is not empty
-        // creates a temp node and sets it to head
-        Node *temp = new Node;
-        temp = head;
-        // uses temp to find the last node
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        // appends the last node with last
-        temp = last;
+// Get node value
+int GetNode(Node *head,int positionFromTail)
+{
+    // create temp to find total length of list
+    Node *temp = head;
+    int n = 0;
+    // loop until end of list to get total length store in n
+    while (temp->next != NULL) {
+        temp = temp->next;
+        n += 1;
     }
-    // returns head of linked list
-    return head
+    // create temp2 to find value at specified node
+    Node *temp2 = head;
+    for(int i=0; i<(n - positionFromTail); i+=1) {
+        temp2 = temp2->next;
+    }
+    return temp2->data;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -4,42 +4,6 @@
  */
 import Foundation
 
-// Sparse Arrays
-// reads line and converts it to an integer
-func readInteger() -> Int {
-    guard let line = readLine() else {
-        fatalError("can't read line")
-    }
-    guard let i = Int(line) else {
-        fatalError("line cannot be converted to int")
-    }
-    return i
-}
-// reads line and converts it to a string
-func readString() -> String {
-    guard let line = readLine() else {
-        fatalError("cant read line")
-    }
-    return line
-}
-let numOfStrings = readInteger()
-// loops over range numOfStrings and readString and puts the string in an array
-let strings = (0..<numOfStrings).map { _ in
-    readString()
-}
-let numOfQueries = readInteger()
-// loops over numOfQueries and readString to get query
-for _ in 0..<numOfQueries {
-    let query = readString()
-    // checks if query equal to any element in strings array, and keeps an accum count
-    let matchingCount = strings.reduce(0) { (accum, string) in
-        accum + (query == string ? 1 : 0)
-    }
-    print(matchingCount)
-}
-
-
-
 
 
 
