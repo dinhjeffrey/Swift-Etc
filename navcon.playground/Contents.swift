33d6191708639108842b7fa663f4636eaf2a7f1d
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-01 15:47:46 -0700

    added navcon playground

---
+++ navcon.playground/Contents.swift
@@ -0,0 +1,11 @@
+// if the VC being segued to is a UIViewController...
+
+extension UIViewController {
+    var contentViewController: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? navcon
+        } else {
+            return self
+        }
+    }
+}
