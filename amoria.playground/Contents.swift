30caf15407bb1f1321e93ae1a8e0858819a9430e
jeffrey dinh <dinhjeffreyy@gmail.com>
2016-08-03 09:00:05 -0700

    updated amoria

--- amoria.playground/Contents.swift
+++ amoria.playground/Contents.swift
@@ -4,69 +4,54 @@
  */
 import Foundation
 
-// returns an integer from one line of standard input
-func readInt() -> Int {
-    guard let line = readLine() else {fatalError("cannot read line")}
-    guard let i = Int(line) else {fatalError("cannot convert line into Int")}
+// Maximum Element
+
+// returns an integer from a line of standard input
+func readInteger() -> Int {
+    guard let line = readLine() else { fatalError("unable to read line") }
+    guard let i = Int(line) else { fatalError("unable to read integer") }
     return i
 }
 
-// returns an array of characters from one line of standard input
-func readChars() -> [Character] {
-    guard let line = readLine() else {fatalError("cannot read line")}
-    return Array(line.characters)
+// returns an array of integers from a line of standard input
+func readIntegers() -> [Int] {
+    guard let line = readLine() else { fatalError("unable to read line") }
+    let integers = line.componentsSeparatedByString(" ").map{ Int($0)! }
+    return integers
 }
 
-// possible brackets
-enum Bracket: Character {
-    case left = "("
-    case right = ")"
-    case leftCurly = "{"
-    case rightCurly = "}"
-    case leftSquare = "["
-    case rightSquare = "]"
-    
-    // if a closing bracket, return its corresponding opening bracket
-    var matchingOpen: Bracket? {
-        switch self {
-        case .right: return left
-        case .rightCurly: return leftCurly
-        case .rightSquare: return leftSquare
-        default: return nil
-        }
-    }
+// creates a stack
+var stack = [Int]()
+
+enum Type: Int {
+    case one = 1, two, three
 }
 
-// checks to see if the line of characters contain all matching backets
-func isBalanced(sequence: [Character]) -> Bool {
-    var stack = [Bracket]()
-    // loop through each bracket
-    for char in sequence {
-        // checks to see if character is a bracket found in enum
-        guard let bracket = Bracket(rawValue: char) else { fatalError("unknown bracket found");continue}
-        // checks to see if bracket is a closing bracket
-        // if opening bracket, append to stack
-        guard let open = bracket.matchingOpen else {stack.append(bracket);continue}
-        // checks to see if last element in stack matches the opening bracket, else it is not balanced
-        guard let last = stack.last where last == open else {return false}
-        // if passes all above, remove the top of stack
+// performs operations on stack depending on input
+func maxElement(input: [Int]) {
+    guard let type = Type(rawValue: input[0]) else { fatalError("unknown type found") }
+    switch type {
+    case .one:
+        let element = input[1]
+        stack.append(element)
+    case .two:
         stack.removeLast()
-    }
-    // returns true if empty, false if not
-    return stack.isEmpty
-}
-func balancedBrackets() {
-    let numSequences = readInt()
-    for _ in 0..<numSequences {
-        let sequence = readChars()
-        // checks tot see if sequence is balanced
-        let balanced = isBalanced(sequence)
-        print(balanced ? "YES" : "NO")
+    case .three:
+        guard let maxElement = stack.maxElement() else { fatalError("stack is empty") }
+        print(maxElement)
     }
 }
 
-balancedBrackets()
+// reads inputs and calls maxElement
+func inputs() {
+    let numQueries = readInteger()
+    for _ in 0..<numQueries {
+        let input = readIntegers()
+        maxElement(input)
+    }
+}
 
+inputs()
 
 
 
